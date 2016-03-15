a89aedaff9ebf26be075630f355243ac93099b4d
Peter Stacho <peterstacho@gmail.com>
2016-03-15 05:20:22 -0400

    Routing Errors

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -15,6 +15,26 @@
         'app.landing',
         'app.layout',
         'app.waitList' 
-    ]);
+    ])
+    .config(configFunction)
+    .run(runFunction);
+    
+    configFunction.$inject = ['$routeProvider'];
+    
+    function configFunction($routeProvider) {
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
+    
+    runFunction.$inject = ['$rootScope', '$location']
+    
+    function runFunction($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+            if (error === "AUTH_REQUIRED") {
+                $location.path('/');
+            }
+        });
+    }
     
  })();
